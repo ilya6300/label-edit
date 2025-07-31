8a1968565442d29b16aae94c72f31df95c82824e
ilya6300 <ilya63001990@mail.ru>
2025-07-31 11:33:16 +0300

    ezpl, import

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -33,7 +33,7 @@ export const Editor = observer(() => {
 		setNoFonts(false)
 	}, [!service.fontsLoading])
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
   // Класс мм сетки
   const [clsMM, setClsMM] = useState("none");
   const [setting, setSetting] = useState(false);
@@ -151,124 +151,124 @@ export const Editor = observer(() => {
     return <BrowserNotSupported />;
   }
 });
-=======
-	// Класс мм сетки
-	const [clsMM, setClsMM] = useState('none')
-	const [setting, setSetting] = useState(false)
-	const [noFonts, setNoFonts] = useState(false)
-	const [importC, setImportC] = useState(false)
-	const [visibleTemplates, setVisibleTemplates] = useState(false)
-	const [flagPreview, setFlagPrevier] = useState(false)
-	const [flagPrinter, setFlagPrinter] = useState(false)
-	const [flagApp, setFlagApp] = useState(true)
-	const [selectedDM, setSelectedDM] = useState(false)
-	if (!browserNotSupportedFlag) {
-		return (
-			<div className='editor_container'>
-				<MessagesContainer />
-				{Memory.exchange ? <ExchangeWithServer /> : <></>}
-				<BtnControl />
-				{!service.fontsLoading && !service.errorNetwork ? (
-					<>
-						{!noFonts ? (
-							<>
-								<BarLabel
-									setVisibleTemplates={setVisibleTemplates}
-									visibleTemplates={visibleTemplates}
-									setFlagPrevier={setFlagPrevier}
-									flagPreview={flagPreview}
-									// clsMM={clsMM}
-									// setClsMM={setClsMM}
-									setImportC={setImportC}
-									setSetting={setSetting}
-									setting={setting}
-									setFlagPrinter={setFlagPrinter}
-									flagApp={flagApp}
-									setFlagApp={setFlagApp}
-								/>
-								{visibleTemplates ? (
-									<GetTemplate setVisibleTemplates={setVisibleTemplates} />
-								) : (
-									<div
-										style={{ color: Theme.color }}
-										className='editor_list_viewer'
-									>
-										{importC ? (
-											<ImportCompanent
-												selectedDM={selectedDM}
-												setSelectedDM={setSelectedDM}
-												setImportC={setImportC}
-											/>
-										) : (
-											<></>
-										)}
-										{selectedDM ? (
-											<ImportContainerDMList setSelectedDM={setSelectedDM} />
-										) : (
-											<></>
-										)}
-										{!service.fontsLoading ? <AddContainer /> : <></>}
-										{Memory.post_download_flag ? (
-											<ContainerPostDownloader />
-										) : (
-											<></>
-										)}
-										<Label
-											flagPreview={flagPreview}
-											setFlagPrevier={setFlagPrevier}
-											clsMM={clsMM}
-											setClsMM={setClsMM}
-										/>
-										{setting ? (
-											<Setting
-												flagPrinter={flagPrinter}
-												setFlagPrinter={setFlagPrinter}
-												flagApp={flagApp}
-												setFlagApp={setFlagApp}
-											/>
-										) : (
-											<></>
-										)}
-									</div>
-								)}
-								{
-									<button
-										style={{ width: '50px' }}
-										onClick={() =>
-											console.log(
-												'objects',
-												toJS(Object.objects),
-												'obj',
-												toJS(Object.obj),
-												'downloaded_template',
-												toJS(Templates.downloaded_template),
-												'Object.download_objects',
-												toJS(Object.download_objects),
-												'this.preview_templates',
-												toJS(Templates.preview_templates),
-												'Object.objects_preview',
-												toJS(Object.objects_preview)
-											)
-										}
-									>
-										Check model
-									</button>
-								}
-							</>
-						) : (
-							<StartAddFonts />
-						)}
-					</>
-				) : !service.fontsLoading && service.errorNetwork ? (
-					<ServerError />
-				) : (
-					<Loader />
-				)}
-			</div>
-		)
-	}
-	if (browserNotSupportedFlag) {
-		return <BrowserNotSupported />
-	}
-})
->>>>>>> origin/drozdov
+// =======
+// 	// Класс мм сетки
+// 	const [clsMM, setClsMM] = useState('none')
+// 	const [setting, setSetting] = useState(false)
+// 	const [noFonts, setNoFonts] = useState(false)
+// 	const [importC, setImportC] = useState(false)
+// 	const [visibleTemplates, setVisibleTemplates] = useState(false)
+// 	const [flagPreview, setFlagPrevier] = useState(false)
+// 	const [flagPrinter, setFlagPrinter] = useState(false)
+// 	const [flagApp, setFlagApp] = useState(true)
+// 	const [selectedDM, setSelectedDM] = useState(false)
+// 	if (!browserNotSupportedFlag) {
+// 		return (
+// 			<div className='editor_container'>
+// 				<MessagesContainer />
+// 				{Memory.exchange ? <ExchangeWithServer /> : <></>}
+// 				<BtnControl />
+// 				{!service.fontsLoading && !service.errorNetwork ? (
+// 					<>
+// 						{!noFonts ? (
+// 							<>
+// 								<BarLabel
+// 									setVisibleTemplates={setVisibleTemplates}
+// 									visibleTemplates={visibleTemplates}
+// 									setFlagPrevier={setFlagPrevier}
+// 									flagPreview={flagPreview}
+// 									// clsMM={clsMM}
+// 									// setClsMM={setClsMM}
+// 									setImportC={setImportC}
+// 									setSetting={setSetting}
+// 									setting={setting}
+// 									setFlagPrinter={setFlagPrinter}
+// 									flagApp={flagApp}
+// 									setFlagApp={setFlagApp}
+// 								/>
+// 								{visibleTemplates ? (
+// 									<GetTemplate setVisibleTemplates={setVisibleTemplates} />
+// 								) : (
+// 									<div
+// 										style={{ color: Theme.color }}
+// 										className='editor_list_viewer'
+// 									>
+// 										{importC ? (
+// 											<ImportCompanent
+// 												selectedDM={selectedDM}
+// 												setSelectedDM={setSelectedDM}
+// 												setImportC={setImportC}
+// 											/>
+// 										) : (
+// 											<></>
+// 										)}
+// 										{selectedDM ? (
+// 											<ImportContainerDMList setSelectedDM={setSelectedDM} />
+// 										) : (
+// 											<></>
+// 										)}
+// 										{!service.fontsLoading ? <AddContainer /> : <></>}
+// 										{Memory.post_download_flag ? (
+// 											<ContainerPostDownloader />
+// 										) : (
+// 											<></>
+// 										)}
+// 										<Label
+// 											flagPreview={flagPreview}
+// 											setFlagPrevier={setFlagPrevier}
+// 											clsMM={clsMM}
+// 											setClsMM={setClsMM}
+// 										/>
+// 										{setting ? (
+// 											<Setting
+// 												flagPrinter={flagPrinter}
+// 												setFlagPrinter={setFlagPrinter}
+// 												flagApp={flagApp}
+// 												setFlagApp={setFlagApp}
+// 											/>
+// 										) : (
+// 											<></>
+// 										)}
+// 									</div>
+// 								)}
+// 								{
+// 									<button
+// 										style={{ width: '50px' }}
+// 										onClick={() =>
+// 											console.log(
+// 												'objects',
+// 												toJS(Object.objects),
+// 												'obj',
+// 												toJS(Object.obj),
+// 												'downloaded_template',
+// 												toJS(Templates.downloaded_template),
+// 												'Object.download_objects',
+// 												toJS(Object.download_objects),
+// 												'this.preview_templates',
+// 												toJS(Templates.preview_templates),
+// 												'Object.objects_preview',
+// 												toJS(Object.objects_preview)
+// 											)
+// 										}
+// 									>
+// 										Check model
+// 									</button>
+// 								}
+// 							</>
+// 						) : (
+// 							<StartAddFonts />
+// 						)}
+// 					</>
+// 				) : !service.fontsLoading && service.errorNetwork ? (
+// 					<ServerError />
+// 				) : (
+// 					<Loader />
+// 				)}
+// 			</div>
+// 		)
+// 	}
+// 	if (browserNotSupportedFlag) {
+// 		return <BrowserNotSupported />
+// 	}
+// })
+// >>>>>>> origin/drozdov
