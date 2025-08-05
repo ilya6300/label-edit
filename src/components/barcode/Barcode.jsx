ebb195949ac442719673ae0f01ba1f77ebbf7baa
ilya6300 <ilya63001990@mail.ru>
2025-08-05 11:07:38 +0300

    barcode useeffect

--- src/components/barcode/Barcode.jsx
+++ src/components/barcode/Barcode.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import bwipjs from "bwip-js";
 import Memory from "../../store/Memory";
 import { observer } from "mobx-react-lite";
@@ -7,38 +7,68 @@ export const Barcode = observer(({ obj, add_canvas }) => {
   const [body, setBody] = useState(
     obj.fakeBody.length >= 13 ? obj.fakeBody : "0000000000000"
   );
-  try {
-    if (obj.typeBarcode === "ean13" || obj.typeBarcode === "code128") {
-      let canvas = bwipjs.toCanvas("mycanvas" + obj.id + add_canvas, {
-        // scale: 2, // 3x scaling factor
-        scaleX: obj.w,
-        scaleY: 2,
-        bcid: obj.typeBarcode, // Barcode type
-        text: body, // Text to encode
-        height: obj.h * 2,
-      });
-    } else if (obj.typeBarcode === "datamatrix") {
-      let canvas = bwipjs.toCanvas("mycanvas" + obj.id + add_canvas, {
-        // scale: w, // 3x scaling factor
-        // scaleX: 1,
-        // scaleY: 2,
-        bcid: "datamatrix", // Barcode type
-        text: "^FNC1" + obj.fakeBody, // Text to encode
-        height: obj.w * obj.min_size,
-        width: obj.w * obj.min_size,
-        parsefnc: true,
-      });
-    } else {
-      let canvas = bwipjs.toCanvas("mycanvas" + obj.id + add_canvas, {
-        bcid: obj.typeBarcode, // Barcode type
-        text: obj.fakeBody, // Text to encode
-        height: obj.h,
-        width: obj.w,
-      });
+  useEffect(() => {
+    try {
+      if (obj.typeBarcode === "ean13" || obj.typeBarcode === "code128") {
+        let canvas = bwipjs.toCanvas("mycanvas" + obj.id + add_canvas, {
+          scaleX: obj.w,
+          scaleY: 2,
+          bcid: obj.typeBarcode,
+          text: body,
+          height: obj.h * 2,
+        });
+      } else if (obj.typeBarcode === "datamatrix") {
+        let canvas = bwipjs.toCanvas("mycanvas" + obj.id + add_canvas, {
+          bcid: "datamatrix",
+          text: "^FNC1" + obj.fakeBody,
+          height: obj.w * obj.min_size,
+          width: obj.w * obj.min_size,
+          parsefnc: true,
+        });
+      } else {
+        let canvas = bwipjs.toCanvas("mycanvas" + obj.id + add_canvas, {
+          bcid: obj.typeBarcode,
+          text: obj.fakeBody,
+          height: obj.h,
+          width: obj.w,
+        });
+      }
+    } catch (e) {
+      console.error("Error generating barcode:", e);
     }
-  } catch (e) {
-    // `e` may be a string or Error object
-  }
+  }, [obj, add_canvas, body]);
+  // try {
+  //   if (obj.typeBarcode === "ean13" || obj.typeBarcode === "code128") {
+  //     let canvas = bwipjs.toCanvas("mycanvas" + obj.id + add_canvas, {
+  //       // scale: 2, // 3x scaling factor
+  //       scaleX: obj.w,
+  //       scaleY: 2,
+  //       bcid: obj.typeBarcode, // Barcode type
+  //       text: body, // Text to encode
+  //       height: obj.h * 2,
+  //     });
+  //   } else if (obj.typeBarcode === "datamatrix") {
+  //     let canvas = bwipjs.toCanvas("mycanvas" + obj.id + add_canvas, {
+  //       // scale: w, // 3x scaling factor
+  //       // scaleX: 1,
+  //       // scaleY: 2,
+  //       bcid: "datamatrix", // Barcode type
+  //       text: "^FNC1" + obj.fakeBody, // Text to encode
+  //       height: obj.w * obj.min_size,
+  //       width: obj.w * obj.min_size,
+  //       parsefnc: true,
+  //     });
+  //   } else {
+  //     let canvas = bwipjs.toCanvas("mycanvas" + obj.id + add_canvas, {
+  //       bcid: obj.typeBarcode, // Barcode type
+  //       text: obj.fakeBody, // Text to encode
+  //       height: obj.h,
+  //       width: obj.w,
+  //     });
+  //   }
+  // } catch (e) {
+  //   // `e` may be a string or Error object
+  // }
   return (
     <>
       <canvas
